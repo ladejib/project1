54447b0031a1293a4b52aa48b53b67647c6ef91f
ladejib <ladejibiobaku@gmail.com>
2025-02-16 06:02:27 -0500

    updated container and network

--- src/app.py
+++ src/app.py
@@ -23,5 +23,5 @@ def home():
     return render_template_string(template, bg_color="lightblue")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
